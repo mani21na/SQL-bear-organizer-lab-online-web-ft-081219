2541024170ded25c6d480dc02ac06f3caedcfb0a
Hanna Lee <mani21na@gmail.com>
2019-09-18 22:12:38 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
-INSERT INTO bears (name) VALUES ();
-INSERT INTO bears (name) VALUES ();
+INSERT INTO bears (name) VALUES ("Mr. Chocolate");
+INSERT INTO bears (name) VALUES ("");
 INSERT INTO bears (name) VALUES ();
 INSERT INTO bears (name) VALUES ();
 INSERT INTO bears (name) VALUES ();
